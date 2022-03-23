855fcfe77e0614ba004b06d4f0741c238775a6d1
Vladislavs <vladislavs1992@gmail.com>
2022-03-23 22:19:27 +0200

    [Vlad] Added code and readme

---
+++ src/test/java/vlad/TheTask/TheTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package vlad.TheTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
